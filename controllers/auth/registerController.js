e71d1d0db64208e8b9f2e760008478bd1069b029
vikash93825 <vikashkumar.kk@gmail.com>
2023-03-16 22:02:07 +0530

    setup for project

---
+++ controllers/auth/registerController.js
@@ -0,0 +1,7 @@
+const registerController = {
+  register(req, res, next) {
+    res.json({msg:"hello from json"})
+  },
+};
+
+export default registerController;
---
+++ server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import { APP_PORT } from "./config";
+
+const app = express();
+
+import routes from "./routes";
+
+app.use("/api", routes);
+
+app.listen(APP_PORT, () => console.log(`Listening port ${APP_PORT}`));
